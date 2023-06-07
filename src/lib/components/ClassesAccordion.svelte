5afa191ea8334f699655735f1bc580732b4f169b
Kireobat <erik.flatabo@gmail.com>
2023-06-07 17:59:53 +0200

    ukeplaner

---
+++ src/lib/components/ClassesAccordion.svelte
@@ -0,0 +1,204 @@
+<script lang="ts">
+  import { Accordion } from "stwui";
+
+  let open = "";
+
+  function handleClick(item: string) {
+    if (open === item) {
+      open = "";
+    } else {
+      open = item;
+    }
+  }
+
+  let items = [
+    {
+      name: "1. Trinn",
+      content: {
+        pdf: {
+          link: "src/lib/assets/pdf/T1-U23.pdf",
+          text: "Last ned PDF",
+        },
+      },
+    },
+    {
+      name: "2. Trinn",
+      content: {
+        pdf: {
+          link: "src/lib/assets/pdf/T2-U23.pdf",
+          text: "Last ned PDF",
+        },
+      },
+    },
+    {
+      name: "3. Trinn",
+      content: {
+        pdf: {
+          link: "src/lib/assets/pdf/T3-U23.pdf",
+          text: "Last ned PDF",
+        },
+      },
+    },
+    {
+      name: "4. Trinn",
+      content: {
+        pdf: {
+          link: "src/lib/assets/pdf/T4-U23.pdf",
+          text: "Last ned PDF",
+        },
+      },
+    },
+    {
+      name: "5. Trinn",
+      content: {
+        pdf: {
+          link: "src/lib/assets/pdf/T5-U23.pdf",
+          text: "Last ned PDF",
+        },
+      },
+    },
+    {
+      name: "6. Trinn",
+      content: {
+        pdf: {
+          link: "src/lib/assets/pdf/T6-U23.pdf",
+          text: "Last ned PDF",
+        },
+      },
+    },
+    {
+      name: "7. Trinn",
+      content: {
+        pdf: {
+          link: "src/lib/assets/pdf/T7-U23.pdf",
+          text: "Last ned PDF",
+        },
+      },
+    },
+    {
+      name: "8A",
+      content: {
+        pdf: {
+          link: "src/lib/assets/pdf/T8-U23-KA.pdf",
+          text: "Last ned PDF",
+        },
+      },
+    },
+    {
+      name: "8B",
+      content: {
+        pdf: {
+          link: "src/lib/assets/pdf/T8-U23-KB.pdf",
+          text: "Last ned PDF",
+        },
+      },
+    },
+    {
+      name: "8C",
+      content: {
+        pdf: {
+          link: "src/lib/assets/pdf/T8-U23-KC.pdf",
+          text: "Last ned PDF",
+        },
+      },
+    },
+    {
+      name: "8D",
+      content: {
+        pdf: {
+          link: "src/lib/assets/pdf/T8-U23-KD.pdf",
+          text: "Last ned PDF",
+        },
+      },
+    },
+    {
+      name: "9A",
+      content: {
+        pdf: {
+          link: "src/lib/assets/pdf/T9-U23-KA.pdf",
+          text: "Last ned PDF",
+        },
+      },
+    },
+    {
+      name: "9B",
+      content: {
+        pdf: {
+          link: "src/lib/assets/pdf/T9-U23-KB.pdf",
+          text: "Last ned PDF",
+        },
+      },
+    },
+    {
+      name: "9C",
+      content: {
+        pdf: {
+          link: "src/lib/assets/pdf/T9-U23-KC.pdf",
+          text: "Last ned PDF",
+        },
+      },
+    },
+    {
+      name: "9D",
+      content: {
+        pdf: {
+          link: "src/lib/assets/pdf/T9-U23-KD.pdf",
+          text: "Last ned PDF",
+        },
+      },
+    },
+    {
+      name: "10A",
+      content: {
+        pdf: {
+          link: "src/lib/assets/pdf/T10-U23-KA.pdf",
+          text: "Last ned PDF",
+        },
+      },
+    },
+    {
+      name: "10B",
+      content: {
+        pdf: {
+          link: "src/lib/assets/pdf/T10-U23-KB.pdf",
+          text: "Last ned PDF",
+        },
+      },
+    },
+    {
+      name: "10C",
+      content: {
+        pdf: {
+          link: "src/lib/assets/pdf/T10-U23-KC.pdf",
+          text: "Last ned PDF",
+        },
+      },
+    },
+    {
+      name: "10D",
+      content: {
+        pdf: {
+          link: "src/lib/assets/pdf/T10-U23-KD.pdf",
+          text: "Last ned PDF",
+        },
+      },
+    },
+  ];
+</script>
+
+<Accordion>
+  {#each items as item}
+    <Accordion.Item open={open === item.name}>
+      <Accordion.Item.Title
+        slot="title"
+        on:click={() => handleClick(item.name)}
+      >
+        {item.name}
+      </Accordion.Item.Title>
+      <Accordion.Item.Content slot="content" class="ml-4 mb-4">
+        <h3>Ukeplan</h3>
+        <a href={item.content.pdf.link} download>{item.content.pdf.text}</a>
+      </Accordion.Item.Content>
+    </Accordion.Item>
+  {/each}
+</Accordion>
--- src/routes/dashboard/+page.svelte
+++ src/routes/dashboard/+page.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
   import { onMount } from "svelte";
   import Menu from "$lib/components/Menu.svelte";
+  import ClassesAccordion from "$lib/components/ClassesAccordion.svelte";
 
   let token: any;
   let loggedIn = false;
@@ -41,19 +42,20 @@
 
 {#if loggedIn}
   <div class="grid grid-cols-10 grid-rows-5">
-    <div class="col-span-10 flex justify-between bg-slate-100">
+    <div class="col-span-10 flex justify-between bg-slate-100 h-16">
       <div><h1>Itslearning 2</h1></div>
       <div class="flex">
         <h3>Welcome, {user.firstname}</h3>
         <img alt="aaa" src="/{user.username}/picture" />
       </div>
     </div>
-    <div class="row-span-4 row-start-2 bg-slate-100 w-12">
+    <div class="row-span-4 row-start-2 bg-slate-100 w-12 -mt-40">
       <Menu />
     </div>
-    <div class="col-span-9 row-span-4 row-start-2 ml-4">
-      <h1>dashboard</h1>
-      <p>bilder og ukeplaner</p>
+    <div class="col-span-9 row-span-4 row-start-2 ml-4 -mt-40">
+      {#if true}
+        <ClassesAccordion />
+      {/if}
     </div>
   </div>
 {/if}
