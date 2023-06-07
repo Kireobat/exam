af068b8d8a3635e418bf6b5b40bd8ea584b80ce7
Kireobat <erik.flatabo@gmail.com>
2023-06-07 11:19:36 +0200

    login things

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,12 +1,30 @@
 <script lang="ts">
-  import { Input } from "stwui";
+  import { Input, Button } from "stwui";
+
+  let loading = false;
+
+  function toggleLoading() {
+    loading = !loading;
+  }
 </script>
 
 <main>
-  <Input name="input">
-    <Input.Label slot="label">Username</Input.Label>
-  </Input>
-  <Input type="password" name="input" showPasswordToggle>
-    <Input.Label slot="label">Password</Input.Label>
-  </Input>
+  <div class="flex justify-center">
+    <div class="w-96 flex flex-col">
+      <div class="my-4">
+        <Input name="input">
+          <Input.Label slot="label">Username</Input.Label>
+        </Input>
+        <Input type="password" name="input" showPasswordToggle>
+          <Input.Label slot="label">Password</Input.Label>
+        </Input>
+      </div>
+      <div>
+        <Button type="primary" {loading} on:click={toggleLoading}>Login</Button>
+        <Button type="ghost" {loading} on:click={toggleLoading}
+          >Registrer</Button
+        >
+      </div>
+    </div>
+  </div>
 </main>
